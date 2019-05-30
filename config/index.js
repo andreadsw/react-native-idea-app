43ac20ac3459dd27c27b5815bd90e88960f503a8
andreadsw <andrea.dossantosw@gmail.com>
2019-05-30 19:00:50 +0100

    Changing config.

--- config/index.js
+++ config/index.js
@@ -1,12 +1,12 @@
 //Firebase API configuration
 
 const config = {
-    apiKey: 'AIzaSyBFgAJQ3OuBL8L37kGyJ1BaCF20vcULybc',
-    authDomain: 'app-react-native-auth.firebaseapp.com',
-    databaseURL: 'https://app-react-native-auth.firebaseio.com',
-    projectId: 'app-react-native-auth',
-    storageBucket: 'app-react-native-auth.appspot.com',
-    messagingSenderId: '957596588375'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
 };
 
 export default config;
